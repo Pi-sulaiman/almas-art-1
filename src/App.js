99eee86ba0c88c283d0b78d130c2c90e8d1f91fc
Pi Sulaiman <pi.sulaiman12@gmail.com>
2023-09-26 19:28:33 +0530

    add logo and other things

--- src/App.js
+++ src/App.js
@@ -1,34 +1,78 @@
-import * as THREE from 'three'
-import { useEffect, useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { useCursor, MeshReflectorMaterial, Image, Text, Environment } from '@react-three/drei'
-import { useRoute, useLocation } from 'wouter'
-import { easing } from 'maath'
-import { suspend } from 'suspend-react'
-import getUuid from 'uuid-by-string'
-// import myGif from  '../public/12.gif'
-const city = import('./b.exr').then((module) => module.default)
+import React, { useEffect, useRef, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useRoute, useLocation } from 'wouter';
+import { easing } from 'maath';
+import { suspend } from 'suspend-react';
+import getUuid from 'uuid-by-string';
+import { MeshReflectorMaterial, Image, Text, Environment, useCursor } from '@react-three/drei';
+import * as THREE from 'three';  // Make sure to import THREE.
+import logo from './logo.svg'
 
-const GOLDENRATIO = 1.61803398875
+const city = import('./a.exr').then((module) => module.default);
+
+const GOLDENRATIO = 1.61803398875;
 
 export const App = ({ images }) => {
-  const mqlRef = useRef()
-  mqlRef.current ??= window.matchMedia('(orientation:portrait)')
-  const [portrait, setPortrait] = useState(mqlRef.current.matches)
+  const mqlRef = useRef();
+  mqlRef.current ??= window.matchMedia('(orientation:portrait)');
+  const [portrait, setPortrait] = useState(mqlRef.current.matches);
+  
+
   useEffect(() => {
-    const handleChange = (e) => setPortrait(e.matches)
-    mqlRef.current.addEventListener('change', handleChange)
-    return () => mqlRef.current.removeEventListener('change', handleChange)
-  }, [])
-  console.log(portrait)
-    
+    const handleChange = (e) => setPortrait(e.matches);
+    enterFullScreen()
+    mqlRef.current.addEventListener('change', handleChange);
+    return () => mqlRef.current.removeEventListener('change', handleChange);
+  }, []);
+
+  console.log(portrait, "dlkjfglsj");
+  const [isFullScreen, setIsFullScreen] = useState(false);
+
+  const enterFullScreen = async () => {
+    try {
+      const element = document.documentElement;
+      if (element.requestFullscreen) {
+        await element.requestFullscreen();
+      } else if (element.mozRequestFullScreen) {
+        await element.mozRequestFullScreen();
+      } else if (element.webkitRequestFullscreen) {
+        await element.webkitRequestFullscreen();
+      } else if (element.msRequestFullscreen) {
+        await element.msRequestFullscreen();
+      }
+      setIsFullScreen(true);
+    } catch (error) {
+      console.error('Error entering full screen:', error);
+    }
+  };
+
+  const exitFullScreen = () => {
+    try {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      }
+      setIsFullScreen(false);
+    } catch (error) {
+      console.error('Error exiting full screen:', error);
+    }
+  };
+
+
   return portrait ? (
-    
     <div className='rotate'>
-     <img src='/12.gif' alt="my-gif" />
+      <img src='/12.gif' alt="my-gif" />
+
     </div>
+    
   ) : (
-    <Canvas dpr={[1, 1.5]} camera={{ fov: 70, position: [0, 2, 15] }}>
+    
+    <Canvas  onClick={enterFullScreen} dpr={[1, 1.5]} camera={{ fov: 70, position: [0, 2, 15] }}>
       <color attach="background" args={['#191920']} />
       <fog attach="fog" args={['#191920', 0, 15]} />
       <group position={[0, -0.5, 0]}>
@@ -49,11 +93,15 @@ export const App = ({ images }) => {
           />
         </mesh>
       </group>
+
       {/* <Environment preset="city" /> */}
       {/* <Environment files="./123.hdr" /> */}
-      <Environment files={suspend(city)} />
+      <Environment files={suspend(city)} /> 
+      {/* {startTransition(() => (
+        <Environment files={suspend(city)} />
+      ))} */}
     </Canvas>
-  )
+  );
 }
 
 function Frames({ images, q = new THREE.Quaternion(), p = new THREE.Vector3() }) {
@@ -76,34 +124,41 @@ function Frames({ images, q = new THREE.Quaternion(), p = new THREE.Vector3() })
     easing.damp3(state.camera.position, p, 0.4, dt)
     easing.dampQ(state.camera.quaternion, q, 0.4, dt)
   })
+
+  
   return (
     <group
       ref={ref}
-      onClick={(e) => (e.stopPropagation(), setLocation(clicked.current === e.object ? '/' : '/item/' + e.object.name))}
+      // data= {props.text}
+      onClick={(e) => (e.stopPropagation(), setLocation(clicked.current === e.object ? '/' : '/item/' + e.object.text),console.log(e))}
       onPointerMissed={() => setLocation('/')}>
       {images.map((props) => <Frame key={props.url} {...props} /> /* prettier-ignore */)}
     </group>
+
   )
 }
 
 function Frame({ url, c = new THREE.Color(), ...props }) {
+  // console.log(props);
   const image = useRef()
   const frame = useRef()
   const [, params] = useRoute('/item/:id')
   const [hovered, hover] = useState(false)
   const [rnd] = useState(() => Math.random())
   const name = getUuid(url)
-  const isActive = params?.id === name
+  const isActive = params?.id === props.text
+  console.log("params?.id === props.text",params?.id === props.text,params?.id ,props.text)
   useCursor(hovered)
-  useFrame((state, dt) => {
+  useFrame((state, dt) => { 
     image.current.material.zoom = 2 + Math.sin(rnd * 10000 + state.clock.elapsedTime / 3) / 2
     easing.damp3(image.current.scale, [0.85 * (!isActive && hovered ? 0.995 : 1), 0.9 * (!isActive && hovered ? 0.995 : 1), 1], 0.1, dt)
-    easing.dampC(frame.current.material.color, hovered ? '#00ff00' : 'white', 0.1, dt)
+    easing.dampC(frame.current.material.color, hovered ? '#8176FF' : 'white', 0.1, dt)
   })
   return (
     <group {...props}>
       <mesh
-        name={name}
+        name={props.text}
+        text={props.text}
         onPointerOver={(e) => (e.stopPropagation(), hover(true))}
         onPointerOut={() => hover(false)}
         scale={[1, GOLDENRATIO, 0.05]}
@@ -117,8 +172,11 @@ function Frame({ url, c = new THREE.Color(), ...props }) {
         <Image raycast={() => null} ref={image} position={[0, 0, 0.7]} url={url} />
       </mesh>
       <Text maxWidth={0.1} anchorX="left" anchorY="top" position={[0.55, GOLDENRATIO, 0]} fontSize={0.025}>
-        {name.split('-').join(' ')}
+        {/* {name.split('-').join(' ')} */}
+        {props.text}
+        {props.text1}
       </Text>
     </group>
   )
+  
 }
--- src/index.js
+++ src/index.js
@@ -15,18 +15,18 @@ import AI from "./9.jpg"
 // const pexel = (id) => `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`
 const images = [
   // Front
-  { position: [0, 0, 1.5], rotation: [0, 0, 0], url: AA },
+  { position: [0, 0, 1.5], rotation: [0, 0, 0], url: AA, text:"Art-1" , text1:"    By Almas"},
   // Back
-  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: AH  },
-  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: AI },
+  { position: [-0.8, 0, -0.6], rotation: [0, 0, 0], url: AH , text:"Art-2" , text1:"    By Almas"  },
+  { position: [0.8, 0, -0.6], rotation: [0, 0, 0], url: AI , text:"Art-3" , text1:"    By Almas" },
   // Left
-  { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: AF },
-  { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: AE },
-  { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: AB },
+  { position: [-1.75, 0, 0.25], rotation: [0, Math.PI / 2.5, 0], url: AF , text:"Art-4" , text1:"    By Almas" },
+  { position: [-2.15, 0, 1.5], rotation: [0, Math.PI / 2.5, 0], url: AE , text:"Art-5" , text1:"    By Almas" },
+  { position: [-2, 0, 2.75], rotation: [0, Math.PI / 2.5, 0], url: AB , text:"Art-6" , text1:"    By Almas" },
   // Right
-  { position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: AG },
-  { position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: AD },
-  { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: AC }
+  { position: [1.75, 0, 0.25], rotation: [0, -Math.PI / 2.5, 0], url: AG , text:"Art-7" , text1:"    By Almas" },
+  { position: [2.15, 0, 1.5], rotation: [0, -Math.PI / 2.5, 0], url: AD , text:"Art-8" , text1:"    By Almas" },
+  { position: [2, 0, 2.75], rotation: [0, -Math.PI / 2.5, 0], url: AC , text:"Art-9" , text1:"    By Almas" }
 ]
 
 createRoot(document.getElementById('root')).render(<App images={images} />)
